05f8d7721f44561b5e850b2bd4015a8305456d2f
Richard <72392252+richardcollinsDkIT@users.noreply.github.com>
2022-12-26 18:30:12 +0000

    added files

---
+++ main.py
@@ -0,0 +1,22 @@
+import requests
+import schedule
+import time
+
+def send_message():
+    messages = ['Good morning Saidhbh', 'Good morning :)', 'Not awake rn but ']
+
+    res = requests.post('https://textbelt.com/text', {
+        'phone': '+353857517853',
+        'message': 'Good morning Saidhbh',
+        'key': 'textbelt',
+    })
+    print(res.json())
+
+
+schedule.every().day.at("07:00").do(send_message)
+
+if __name__ == '__main__':
+    while True:
+        schedule.run_pending()
+        time.sleep(60)
+   
