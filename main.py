f52255c36890f68423d4cf37235a8d1195ee9394
RKEC <d00230925@student.dkie.ie>
2022-12-27 19:18:13 +0000

    Added env and random time

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 import requests
 import schedule
 import time
+import random
+import env
 
 def send_message():
-    messages = ['Good morning Saidhbh', 'Good morning :)', 'Not awake rn but ']
+    
+    messages = env.MESSAGES
+
+    message = messages[random.randint(0, len(messages)-1)] + " - Richard"
+    print("Message: {}".format(message))
 
     res = requests.post('https://textbelt.com/text', {
-        'phone': num,
-        'message': messages,
+        'phone': env.PHONE,
+        'message': message,
         'key': 'textbelt',
     })
     print(res.json())
+    next_time()
+    return schedule.CancelJob
 
+def next_time():
+    time_str = '{:02d}:{:02d}'.format(7, random.randint(0, 59))
+    schedule.clear()
+    print("Scheduled for {}".format(time_str))
+    schedule.every().day.at(time_str).do(send_message)
 
-schedule.every().day.at("07:00").do(send_message)
+next_time()
 
 if __name__ == '__main__':
     while True:
